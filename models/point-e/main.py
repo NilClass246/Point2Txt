df887f6fcc2dc1c4f34ed2cc7dbe6e88c62d857b
Yuntian Liu <liu2002246@gmail.com>
2025-11-14 13:52:07 -0500

    remove PointCLIPV2

---
+++ models/point-e/main.py
@@ -0,0 +1,19 @@
+import torch
+from tqdm.auto import tqdm
+
+from point_e.diffusion.configs import DIFFUSION_CONFIGS, diffusion_from_config
+from point_e.diffusion.sampler import PointCloudSampler
+from point_e.models.download import load_checkpoint
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.plotting import plot_point_cloud
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# TODO: complete specification
+def load_point_e(training_args):
+    pass
+
+# TODO: complete test
+if __name__ == "__main__":
+    pass
