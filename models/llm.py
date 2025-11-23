f5033024513ffe6ada7f01eb5803c7598ef1cab4
Oleksii Nakhod <26633244+oleksii-nakhod@users.noreply.github.com>
2025-11-23 08:26:00 -0500

    Fine-tune GPT-2

--- models/llm.py
+++ models/llm.py
@@ -15,7 +15,7 @@ def load_gpt2(device):
     print("GPT-2 vocab size:", len(tokenizer))
     print("GPT-2 hidden size:", gpt2.config.n_embd)
 
-    freeze_gpt2 = True  # set False if you want to fine-tune GPT-2
+    freeze_gpt2 = False  # set False if you want to fine-tune GPT-2
 
     if freeze_gpt2:
         for param in gpt2.parameters():
