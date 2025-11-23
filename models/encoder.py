c84948387ec44b30ae55357fd1d6ee8f7883ea5c
Oleksii Nakhod <26633244+oleksii-nakhod@users.noreply.github.com>
2025-11-23 10:51:14 -0500

    Use yaml.safe_load()

--- models/encoder.py
+++ models/encoder.py
@@ -7,7 +7,7 @@ from easydict import EasyDict
 def cfg_from_yaml_file(cfg_file):
     config = EasyDict()
     with open(cfg_file, 'r') as f:
-        new_config = yaml.load(f, Loader=yaml.FullLoader)
+        new_config = yaml.safe_load(f) 
     merge_new_config(config=config, new_config=new_config)
     return config
 
@@ -16,10 +16,7 @@ def merge_new_config(config, new_config):
         if not isinstance(val, dict):
             if key == '_base_':
                 with open(new_config['_base_'], 'r') as f:
-                    try:
-                        val = yaml.load(f, Loader=yaml.FullLoader)
-                    except:
-                        val = yaml.load(f)
+                    val = yaml.safe_load(f)
                 config[key] = EasyDict()
                 merge_new_config(config[key], val)
             else:
